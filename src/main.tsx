48f4b1245b8e252c1c4bae971ecc400e2d60287a
laoyutong <1262145400@qq.com>
2021-06-23 21:05:03 +0800

    feat: add fastclick

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { FastClick } from "fastclick";
+
+FastClick.attach(document.body);
 
 ReactDOM.render(
   <React.StrictMode>
